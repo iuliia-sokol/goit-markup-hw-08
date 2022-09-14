8cbd2ed980baf590da6e168e1b602e024f31949e
Iuliia Sokol <avare779@gmail.com>
2022-09-14 20:53:20 +0300

    hide class saved in separated file, reset form option added to JS code

--- js/modal-form(my).js
+++ js/modal-form(my).js
@@ -2,6 +2,7 @@ const openModalBtn = document.querySelector(".hero__btn");
 const closeModalBtn = document.querySelector(".close-btn");
 const modal = document.querySelector(".modal-overlay");
 const modalWindow = document.querySelector(".modal-window");
+const modalForm = document.querySelector(".call-form");
 
 openModalBtn.addEventListener("click", function () {
   modal.classList.remove("is-hidden");
@@ -9,17 +10,20 @@ openModalBtn.addEventListener("click", function () {
 
 closeModalBtn.addEventListener("click", function () {
   modal.classList.toggle("is-hidden");
+  modalForm.reset();
 });
 
 modal.addEventListener("click", (e) => {
   const closeModal = e.composedPath().includes(modalWindow);
   if (!closeModal) {
     modal.classList.add("is-hidden");
+    modalForm.reset();
   }
 });
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     modal.classList.add("is-hidden");
+    modalForm.reset();
   }
 });
